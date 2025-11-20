703a5c30bca693e5aa8845b7fe9f18765fb45d78
Sonam Sherpa <sherpa.sjs@gmail.com>
2025-11-20 10:38:06 -0800

    slider UI updated

--- app/components/MenuSection.tsx
+++ app/components/MenuSection.tsx
@@ -173,7 +173,7 @@ const MenuSection = () => {
               <svg className="w-5 h-5 mr-1 text-yellow-200 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
               </svg>
-              <span className="drop-shadow">Slide to see more</span>
+              <span className="drop-shadow">Slide on image to see more</span>
               <svg className="w-5 h-5 ml-1 text-yellow-200 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
               </svg>
