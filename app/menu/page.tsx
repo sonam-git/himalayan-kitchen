7f9491d71c8609938542cf6bed326ed101e38ae9
Sonam Sherpa <sherpa.sjs@gmail.com>
2025-12-07 23:11:26 -0800

    onlineorder component removed

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import OnlineOrder from '../components/OnlineOrder';
 
 export default function MenuPage() {
   return (
-    <div className=" px-2 mt-30 lg:mt-0">
-      <OnlineOrder />
+    <div className="px-2 mt-30 lg:mt-0">
+      <div className="max-w-7xl mx-auto py-12">
+        <h1 className="text-4xl font-serif font-bold text-center mb-8 text-gray-800 dark:text-white">
+          Our Menu
+        </h1>
+        <p className="text-center text-lg text-gray-600 dark:text-gray-300 mb-8">
+          Menu content coming soon
+        </p>
+      </div>
     </div>
   );
 }
